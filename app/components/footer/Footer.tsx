265c3db71792dbd03b5402572ec08a97286cc467
Riko1213 <batsaikhan1213riko@gmail.com>
2024-05-30 12:23:19 +0800

    Update Footer.tsx

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -10,7 +10,16 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <div className="absolute inset-0 z-[-1]">
+        <Image
+          src="/navbar.png"
+          alt="navbarbg"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16 bg-opacity-70">
       <Container>
         <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
           <FooterList>
