d54a5e261af9118df8e12d0e8af6a02e41175323
Riko1213 <batsaikhan1213riko@gmail.com>
2024-05-30 12:29:28 +0800

    Update NavBar.tsx

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,70 +1,56 @@
 import Link from "next/link";
 import Container from "../Container";
-import FooterList from "./FooterList";
-import { MdFacebook } from "react-icons/md";
-import {
-  AiFillTwitterCircle,
-  AiFillInstagram,
-  AiFillYoutube,
-} from "react-icons/ai";
+import { Redressed } from "next/font/google";
+import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
+import getCurrentUser from "@/actions/getCurrentUser";
+import Categories from "./Categories";
+import SearchBar from "./SearchBar";
+import Image from "next/image";
+const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
-const Footer = () => {
+const NavBar = async () => {
+  const currentUser = await getCurrentUser();
+
   return (
-    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
-      <Container>
-        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
-          <FooterList>
-            <h3 className="text-base font-bold mb-2">Ангилалууд</h3>
-            <Link href="#">Гэр ахуй</Link>
-            <Link href="#">Хүүхдийн</Link>
-            <Link href="#">Машины хэрэгсэл</Link>
-            <Link href="#">Хувцас</Link>
-            <Link href="#">Эрүүл мэнд</Link>
-            <Link href="#">Спорт</Link>
-          </FooterList>
-          <FooterList>
-            <h3 className="text-base font-bold mb-2">Бидэнтэй холбогдох</h3>
-            <Link href="#">Бидэнтэй холбогдох: 89101054, 99415153</Link>
-            <Link href="#">Буцаалт: Үйлчлүүлэгч та бидэнтэй холбогдон захиалгаа буцаах, цуцлах боломжтой.Буцаалтын үед бараа хүргэгдсэн тухайн өдөрт бидэнтэй холбогдож мэдээллэх шаардлагатай.Хэрэв хэрэглэгч бараагаа задлаад, эвдрэл гэмтэл гаргасан тохиолдолд буцаалт хүчингүй.  </Link>
-          </FooterList>
-          <div className="w-full md:w-1/3 mb-6 md:mb-0">
-          <p>&copy; {new Date().getFullYear()} CRYSTAL онлайн шоп</p>
-            <h3 className="text-base font-bold mb-2">Бидний тухай</h3>
-            <p className="mb-2">
-              Бид хэрэглэгчдийнхээ хүсэл, сонирхол, хэрэгцээнд бүрэн нийцсэн бүтээгдэхүүн үйлчилгээг шуурхай хүргэнэ
-              хэмээх эрхэм зорилготойгоор үйл ажиллагаагаа явуулдаг.
-              Өмнөө тавьсан эрхэм зорилгын хүрээнд нэн тэргүүнд “Хэрэглэгч үргэлж сэтгэл хангалуун үлдэх ёстой” зарчмыг баримталдаг.
-              Ширхэг барааг ч Улаанбаатар хот доторх хүргэж өгөх бөгөөд хэрэглэгч та захиалгаа өгсөнөөр дараах өдрүүдээс хүссэн өдөрт нь хүргэж үйлчилнэ. 
-              Орон нутгийн захиалгыг найдвартай хуваарьт шуудангаар хүргэдэг.
-            </p>
-          </div>
-          <FooterList>
-            <h3 className="text-base font-bold mb-2">Биднийг дагаарай</h3>
-            <div className="flex gap-2">
-            <Link href="https://www.facebook.com/Crystalonlinemn">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="https://www.facebook.com/profile.php?id=100068554759141">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="https://www.facebook.com/M.and.M.online.shop.mn">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="https://www.facebook.com/profile.php?id=100077933563661">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="https://www.facebook.com/AriunOnlineClothingShop">
-                <MdFacebook size={24} />
-              </Link>
-              <Link href="#">
-                <AiFillInstagram size={24} />
-              </Link>
+    <div className="relative sticky top-0 w-full z-30 shadow-sm">
+      <div className="absolute inset-0 z-[-1]">
+        <Image
+          src="/navbar.png"
+          alt="navbarbg"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
+      <div className="py-4 border-b-[1px] bg-slate-200 bg-opacity-70">
+        <Container>
+          <div
+            className="
+          flex
+          items-center
+          justify-between
+          gap-3
+          md:gap-0
+          "
+          >
+            <Link
+              href="/"
+              className={`${redressed.className} font-bold text-2xl`}
+            >
+              CRYSTAL Онлайн шоп
+            </Link>
+            <div className="hidden md:block"><SearchBar/></div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <CartCount />
+              <UserMenu currentUser={currentUser} />
             </div>
-          </FooterList>
-        </div>
-      </Container>
-    </footer>
+          </div>
+        </Container>
+      </div>
+      <Categories/>
+    </div>
   );
 };
 
-export default Footer;
+export default NavBar;
